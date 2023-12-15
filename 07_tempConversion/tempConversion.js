497283871ecec9942b102227f7aedb3def027b6e
boxy-pug <151582186+boxy-pug@users.noreply.github.com>
2023-12-15 22:20:44 +0100

    Finished the seventh excercise
    About tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let convertedToCelsius = (num - 32) * (5 / 9);
+  return Math.round(convertedToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let convertedToFarenheit = num * (9 / 5) + 32;
+  return Math.round(convertedToFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
